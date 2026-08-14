edfe472ef8df8153439a4fde2905e849cb86e525
Ihajacker <ihajacker@gmail.com>
2026-08-14 23:14:38 +0800

    fix: 修正 quote_needprefix 语义

--- nonebot_plugin_quote/__init__.py
+++ nonebot_plugin_quote/__init__.py
@@ -737,7 +737,7 @@ gocq_path=/home/xxx/gocq/data/cache'''
         await copy_batch.finish("路径不正确")
     await copy_batch.finish("备份完成")
 
-if plugin_config.quote_needprefix:
+if not plugin_config.quote_needprefix:
     message_handler = on_message(block=False)
     
     @message_handler.handle()
